8ad63ee1d66d8fc0289c202dd1329334bf7bb201
Lol8005 <ekhoos3141@gmail.com>
2024-07-20 01:53:58 +0800

    add TODO

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,8 @@ import Libraries.ArrayList;
 public class Main {
     public static void main(String[] args) {
 
-
+        //TODO: ADT hashmap with multiple key
+        //TODO: Multi algorithm to store data. example: <List> list of donee --> filter with condition --> <Queue> Delivery flow, near to far to near (circle)
 
     }
 }
