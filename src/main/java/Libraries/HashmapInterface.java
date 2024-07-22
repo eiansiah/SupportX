d660bd1a9719515cd6c504a329f76992347e5107
Lol8005 <ekhoos3141@gmail.com>
2024-07-23 01:29:09 +0800

    add hashmap ADT support

---
+++ src/main/java/Libraries/HashmapInterface.java
@@ -0,0 +1,10 @@
+package Libraries;
+
+public interface HashmapInterface<K, V> {
+    void put(K key, V value);
+    V get(K key);
+    void remove(K key);
+    boolean containsKey(K key);
+    int size();
+    void display();
+}
