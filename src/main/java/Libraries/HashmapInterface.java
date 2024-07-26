b77bae5870939c0ad477be254ba648a1ea51ccfd
vivian <kojq-wp22@student.tarc.edu.my>
2024-07-27 02:01:00 +0800

    added clear()

--- src/main/java/Libraries/HashmapInterface.java
+++ src/main/java/Libraries/HashmapInterface.java
@@ -3,7 +3,8 @@ package Libraries;
 public interface HashmapInterface<K, V> {
     void put(K key, V value);
     V get(K key);
-    void remove(K key);
+    boolean remove(K key);
+    void clear();
     boolean containsKey(K key);
     int size();
     void display();
