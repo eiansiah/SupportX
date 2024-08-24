6f56c22527ebbcee2e40d71c36b880da37319ce3
Eddead <121841133+Eddead@users.noreply.github.com>
2024-08-24 15:27:03 +0800

    Update EventFileHandler.java
    Added all interface abstraction method

--- src/main/java/FileHandling/EventFileHandler.java
+++ src/main/java/FileHandling/EventFileHandler.java
@@ -4,11 +4,29 @@ import Libraries.ArrayList;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 //public class EventFileHandler implements FileHandlingInterface<DonationEvent>{
+//     @Override
+//     public void checkAndCreateFile(String filename) {
+//         File file = new File(filename);
+//         try {
+//             if (!file.exists()) {
+//                 file.createNewFile();
+//                 System.out.println("File created: " + filename);
+//             } else {
+//                 //System.out.println("File exists: " + filename);
+//                 System.out.println("System Ready");
+                
+//             }
+//         } catch (IOException e) {
+//             System.err.println("Error creating the file: " + e.getMessage());
+//         }
+//     }
+
 //    @Override
 //    public void saveData(String filename, DonationEvent entity) {
 //
@@ -26,6 +44,11 @@ import java.io.IOException;
 //
 //    @Override
 //    public void updateMultipleData(String filename, ArrayList<DonationEvent> entity) {
+//
+//    }
+
+//    @Override
+//    public void deleteData(String filename, enitity){
 //
 //    }
 //}
