412034133654716df8556619707976ea1c886bfe
Eddead <121841133+Eddead@users.noreply.github.com>
2024-08-24 14:59:38 +0800

    Update FileHandlingInterface.java
    Added function to check if file exist, create one if not and function to delete data, pending request to delete updateData function

--- src/main/java/FileHandling/FileHandlingInterface.java
+++ src/main/java/FileHandling/FileHandlingInterface.java
@@ -4,9 +4,11 @@ import Libraries.ArrayList;
 
 public interface FileHandlingInterface<T> {
     
+    public void checkAndCreateFile(String filename);
     void saveData(String filename, T entity);
     ArrayList<T> readData(String filename);
     void updateData(String filename, T entity);
     void updateMultipleData(String filename, ArrayList<T> entity);
+    void deleteData(String filename, String entity);
 
 }
