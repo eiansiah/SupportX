766906e60ed0f731e71798c99fe75bc18ac27484
Eddead <simhk-wp22@student.tarc.edu.my>
2024-08-26 02:23:20 +0800

    Added filter and sorting function

---
+++ src/main/java/Utilities/DonorFilter.java
@@ -0,0 +1,41 @@
+package Utilities;
+
+import Libraries.ArrayList;
+import Main.Donor;
+
+public class DonorFilter {
+
+
+    // Filter by starting alphabet of the name
+    public static ArrayList<Donor> filterByName(ArrayList<Donor> objects, char startingLetter) {
+        ArrayList<Donor> filteredList = new ArrayList<>();
+        for (Donor obj : objects) {
+            if (obj.getName().toUpperCase().charAt(0) == Character.toUpperCase(startingLetter)) {
+                filteredList.add(obj);
+            }
+        }
+        return filteredList;
+    }
+
+    // Filter by category
+    public static ArrayList<Donor> filterByCategory(ArrayList<Donor> objects, String category) {
+        ArrayList<Donor> filteredList = new ArrayList<>();
+        for (Donor obj : objects) {
+            if (obj.getCategory().equalsIgnoreCase(category)) {
+                filteredList.add(obj);
+            }
+        }
+        return filteredList;
+    }
+
+    // Filter by type
+    public static ArrayList<Donor> filterByType(ArrayList<Donor> objects, String type) {
+        ArrayList<Donor> filteredList = new ArrayList<>();
+        for (Donor obj : objects) {
+            if (obj.getType().equalsIgnoreCase(type)) {
+                filteredList.add(obj);
+            }
+        }
+        return filteredList;
+    }
+}
