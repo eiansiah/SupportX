b13cf20ced5726a645c1d8d04f8843b988bdb692
Eddead <simhk-wp22@student.tarc.edu.my>
2024-08-25 12:23:48 +0800

    Updated validation class

--- src/main/java/Main/Validation.java
+++ src/main/java/Main/Validation.java
@@ -36,16 +36,29 @@ public class Validation {
 
     public static String validatePhone(Scanner scanner) {
         String phone;
-        Pattern phoneFormat = Pattern.compile("^(\\d{10})$");
+        Pattern phoneFormat = Pattern.compile("^(\\d{11})$");
         do {
             System.out.print("Phone: ");
             phone = scanner.nextLine().trim();
             Matcher matcher = phoneFormat.matcher(phone);
             if (!matcher.matches()) {
-                System.err.println("Invalid phone number format. Please enter a valid phone number (e.g.1234567890).");
+                System.err.println("Invalid phone number format. Please enter a valid phone number (e.g.0123456789).");
             }
         } while (!phoneFormat.matcher(phone).matches());
         return phone;
+    }
+
+    public static String validateAddress(Scanner scanner) {
+        String address;
+        do { //Input Validation Loop
+            System.out.print("Address: ");
+            address = scanner.nextLine().trim();  // Read the input as a string and trim any surrounding spaces
+
+            if (address.isEmpty())
+                System.out.println("Empty input detected. Please do not leave address empty.");
+
+        } while (address.isEmpty());
+        return address;
     }
 
     public static String validateCategory(Scanner scanner) {
@@ -79,15 +92,65 @@ public class Validation {
             case 2:
                 category = "Private";
                 break;
-            case 3:
-                category = "Public";
-                break;
             default:
-                category = ""; // This should never happen due to the do-while loop
-                break;
+                category = "Public";
         }
 
         return category;
+    }
+
+    public static String validateItemCategory(Scanner scanner) {
+        String itemCategory;
+        int choice = 0;
+
+        do {
+            if (choice != 0) {
+                System.err.println("Invalid choice. Please select a valid item category required (1-7).");
+            }
+
+            System.out.println("Please select an item category required:");
+            System.out.println("1. Food");
+            System.out.println("2. Beverage");
+            System.out.println("3. Clothing");
+            System.out.println("4. Personal Care");
+            System.out.println("5. Medical Device");
+            System.out.println("6. Medicine");
+            System.out.println("7. Monetary Aid");
+
+            while (!scanner.hasNextInt()) {
+                System.err.println("Invalid input. Please enter a number (1-7).");
+                scanner.next(); // Clear the invalid input
+            }
+
+            choice = scanner.nextInt();
+            scanner.nextLine(); // Consume the newline character
+
+        } while (choice < 1 || choice > 7);
+
+        switch (choice) {
+            case 1:
+                itemCategory = "Food";
+                break;
+            case 2:
+                itemCategory = "Beverage";
+                break;
+            case 3:
+                itemCategory = "Clothing";
+                break;
+            case 4:
+                itemCategory = "Personal Care";
+                break;
+            case 5:
+                itemCategory = "Medical Device";
+                break;
+            case 6:
+                itemCategory = "Medicine";
+                break;
+            default:
+                itemCategory = "Monetary Aid";
+        }
+
+        return itemCategory;
     }
 
     public static String validateType(Scanner scanner) {
@@ -113,6 +176,39 @@ public class Validation {
 
         } while (choice < 1 || choice > 2);
 
+        if (choice == 1) {
+            type = "Individual";
+        } else {
+            type = "Organization";
+        }
+
+        return type;
+    }
+
+    public static String validateDoneeType(Scanner scanner) {
+        String type ;
+        int choice = 0;
+
+        do {
+            if (choice != 0) {
+                System.err.println("Invalid choice. Please select a valid donee type (1-3).");
+            }
+
+            System.out.println("Please select a type:");
+            System.out.println("1. Individual");
+            System.out.println("2. Organization");
+            System.out.println("3. Family");
+
+            while (!scanner.hasNextInt()) {
+                System.err.println("Invalid input. Please enter a number (1-3).");
+                scanner.next(); // Clear the invalid input
+            }
+
+            choice = scanner.nextInt();
+            scanner.nextLine(); // Consume the newline character
+
+        } while (choice < 1 || choice > 3);
+
         switch (choice) {
             case 1:
                 type = "Individual";
@@ -121,12 +217,48 @@ public class Validation {
                 type = "Organization";
                 break;
             default:
-                System.err.println("Invalid choice. Please select a valid type (1-2).");
-                type = null;
-                break;
+                type = "Family";
         }
 
         return type;
+    }
+
+    public static String validateDoneeUrgency(Scanner scanner) {
+        String doneeUrgency;
+        int choice = 0;
+
+        do {
+            if (choice != 0) {
+                System.err.println("Invalid choice. Please select a valid urgency category (1-3).");
+            }
+
+            System.out.println("Please select an urgency category:");
+            System.out.println("1. Low");
+            System.out.println("2. Moderate");
+            System.out.println("3. High");
+
+            while (!scanner.hasNextInt()) {
+                System.err.println("Invalid input. Please enter a number (1-3).");
+                scanner.next(); // Clear the invalid input
+            }
+
+            choice = scanner.nextInt();
+            scanner.nextLine(); // Consume the newline character
+
+        } while (choice < 1 || choice > 3);
+
+        switch (choice) {
+            case 1:
+                doneeUrgency = "Low";
+                break;
+            case 2:
+                doneeUrgency = "Moderate";
+                break;
+            default:
+                doneeUrgency = "High";
+        }
+
+        return doneeUrgency;
     }
 
 }
