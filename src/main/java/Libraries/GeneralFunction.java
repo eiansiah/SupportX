9066981372757afe41df4232c7c7a1efea3b8e14
Eddead <simhk-wp22@student.tarc.edu.my>
2024-08-28 03:16:25 +0800

    add general function

---
+++ src/main/java/Libraries/GeneralFunction.java
@@ -0,0 +1,17 @@
+package Libraries;
+
+import java.util.Scanner;
+
+public class GeneralFunction {
+    public static void enterToContinue(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter to continue");
+        sc.nextLine();
+    }
+
+    public static void repeatPrint(String string, int times){
+        for (int i = 0; i < times; i++) {
+            System.out.print(string);
+        }
+    }
+}
