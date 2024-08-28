8ef05246d1ad78eb6d0906f21d375e88b6f5d3af
Eddead <simhk-wp22@student.tarc.edu.my>
2024-08-28 16:57:09 +0800

    Updated donee files, general function, new validation, message

--- src/main/java/Boundary/DoneeUI.java
+++ src/main/java/Boundary/DoneeUI.java
@@ -1,5 +1,11 @@
 package Boundary;
 
+import Entity.Donee;
+
+import Libraries.ArrayList;
+import Libraries.Color;
+
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class DoneeUI {
@@ -87,4 +93,174 @@ public class DoneeUI {
         return scanner.nextLine().trim();
     }
 
+    public static void deleteDoneeUI(){
+        System.out.println("\nWhich donee would you like to delete?");
+        System.out.print("Please enter the Donee ID (Or X to return) : ");
+    }
+
+    public static void displayIndividualDoneeDetailsUI(String doneeID, String name, String email, String phone, String address, String doneeType, String itemCategory, String doneeUrgency, LocalDate registeredDate){
+        System.out.println("\nDonee Details:");
+        System.out.printf("%-25s: %s%n", "Donee ID", doneeID);
+        System.out.printf("%-25s: %s%n", "Name", name);
+        System.out.printf("%-25s: %s%n", "Email", email);
+        System.out.printf("%-25s: %s%n", "Phone", phone);
+        System.out.printf("%-25s: %s%n", "Address", address);
+        System.out.printf("%-25s: %s%n", "Donee Type", doneeType);
+        System.out.printf("%-25s: %s%n", "Item Category Required", itemCategory);
+        System.out.printf("%-25s: %s%n", "Donee Urgency", doneeUrgency);
+        System.out.printf("%-25s: %s%n", "Registered Date", registeredDate);
+        System.out.print("\n");
+    }
+
+    public static String deleteDoneeConfirmationUI(){
+        System.out.print("Are you sure you want to delete this donee? (Y/N): ");
+        return scanner.nextLine().trim().toUpperCase();
+    }
+
+    public static void deleteDoneeSuccessfulMessage(String doneeId){
+        System.out.println(Color.BRIGHT_GREEN + "\nDonee with ID " + doneeId + " has been deleted successfully!" + Color.RESET);
+    }
+
+    public static void deleteDoneeOperationAbortMessage(){
+        System.out.println(Color.BRIGHT_YELLOW + "\nDelete Operation cancelled." + Color.RESET);
+    }
+
+    public static void modifyDoneeUI(){
+        System.out.print("\nWhich donee would you like to modify?");
+        System.out.print("Please enter the Donee ID: ");
+    }
+
+    public static String modifyTypeUI(){
+        System.out.println("Which part do you want to modify?");
+        System.out.println("1. Name");
+        System.out.println("2. Email");
+        System.out.println("3. Phone");
+        System.out.println("4. Address");
+        System.out.println("5. Donee Type");
+        System.out.println("6. Item Category Required");
+        System.out.println("7. Donee Urgency");
+        System.out.println("X. Return to main menu\n");
+        System.out.print("Please select an option (1-7 or X): ");
+        return scanner.nextLine().trim().toUpperCase();
+    }
+
+    public static void updateDoneeSuccessfulMessage(String doneeId){
+        System.out.println(Color.BRIGHT_GREEN + "\nDonee with ID " + doneeId + " has been updated successfully." + Color.RESET);
+    }
+
+    public static String displayDoneeTableUI(int start, int currentPage, int end, ArrayList<Donee> donees, int totalDonees){
+        System.out.println("\n**LIST OF DONEES (Page " + (currentPage + 1) + ")**\n");
+        System.out.printf("%-10s %-25s %-15s %-15s %n", "Donee ID", "Donee Name", "Donee Phone", "Donee Urgency");
+        System.out.println(String.format("%0" + 65 + "d", 0).replace("0", "-"));
+
+        for (int i = start; i < end; i++) {
+            Donee donee = donees.get(i);
+            System.out.printf("%-10s %-25s %-15s %-15s %n", donee.getDoneeID(), donee.getName(), donee.getPhone(), donee.getDoneeUrgency());
+        }
+        System.out.println(String.format("%0" + 65 + "d", 0).replace("0", "-"));
+
+        System.out.println("\nOptions:");
+
+        if (currentPage > 0) {
+            System.out.println("P - Previous Page");
+        }
+        if (end < totalDonees) {
+            System.out.println("N - Next Page");
+        }
+
+        System.out.println("D - Individual Details (Enter donee ID to view details)");
+        System.out.println("S - Sort Donee");
+        System.out.println("X - Exit");
+        System.out.print("\nSelect an option: ");
+        return scanner.nextLine().trim().toUpperCase();
+    }
+
+    public static void displayFirstPageMessage(){
+        System.out.println("You are already on the first page.");
+    }
+
+    public static void displayLastPageMessage(){
+        System.out.println("You are already on the last page.");
+    }
+
+    public static void displayDoneeUI(){
+        System.out.print("Enter Donee ID to view details: ");
+    }
+
+    public static String sortDoneeUI(){
+        System.out.println("\nSorting Criterion: ");
+        System.out.println("1 - Sort by Donee Urgency (Priority)");
+        System.out.println("2 - Sort by Ascending/Descending Order");
+        System.out.print("\nSelect a sorting criterion: ");
+        return scanner.nextLine().trim();
+    }
+
+    public static String filterDoneeUI(){
+        System.out.println("\nFilter Donees by Category:");
+        System.out.println("1. Donee Type");
+        System.out.println("2. Donee Urgency");
+        System.out.println("3. Return to main menu");
+        System.out.print("\nSelect a category: ");
+        return scanner.nextLine().trim();
+    }
+
+    public static String filterDoneeTypeUI(){
+        System.out.println("\nFilter by Donee Types:");
+        System.out.println("1. Individual");
+        System.out.println("2. Organization");
+        System.out.println("3. Family");
+        System.out.println("4. Back");
+        System.out.print("\nSelect a donee type: ");
+        return scanner.nextLine().trim();
+    }
+
+    public static void individualFilterUI(){
+        System.out.println("\n**Individual Type Donee List**\n");
+        System.out.printf("%-10s %-25s %-15s %-15s %-20s %n", "Donee ID", "Donee Name", "Donee Phone", "Donee Type", "Registered Date");
+        System.out.println(String.format("%0" + 85 + "d", 0).replace("0", "-"));
+    }
+
+    public static void displayFilterDetailsUI(String doneeID, String name, String phone, String doneeTypeOrUrgency, LocalDate registeredDate){
+        System.out.printf("%-10s %-25s %-15s %-15s %-20s %n", doneeID, name, phone, doneeTypeOrUrgency, registeredDate);
+    }
+
+    public static void organizationFilterUI(){
+        System.out.println("\n**Organization Type Donee List**\n");
+        System.out.printf("%-10s %-25s %-15s %-15s %-20s %n", "Donee ID", "Donee Name", "Donee Phone", "Donee Type", "Registered Date");
+        System.out.println(String.format("%0" + 85 + "d", 0).replace("0", "-"));
+    }
+
+    public static void familyFilterUI(){
+        System.out.println("\n**Family Type Donee List**\n");
+        System.out.printf("%-10s %-25s %-15s %-15s %-20s %n", "Donee ID", "Donee Name", "Donee Phone", "Donee Type", "Registered Date");
+        System.out.println(String.format("%0" + 85 + "d", 0).replace("0", "-"));
+    }
+
+    public static String filterUrgencyTypeUI(){
+        System.out.println("\nFilter by Donee Urgency:");
+        System.out.println("1. Low");
+        System.out.println("2. Moderate");
+        System.out.println("3. High");
+        System.out.println("4. Back");
+        System.out.print("\nSelect a donee urgency category: ");
+        return scanner.nextLine().trim();
+    }
+
+    public static void filterLowUrgencyUI(){
+        System.out.println("\n**Low Urgency Donee List**\n");
+        System.out.printf("%-10s %-25s %-15s %-15s %-20s %n", "Donee ID", "Donee Name", "Donee Phone", "Donee Urgency", "Registered Date");
+        System.out.println(String.format("%0" + 85 + "d", 0).replace("0", "-"));
+    }
+
+    public static void filterModerateUrgencyUI(){
+        System.out.println("\n**Moderate Urgency Donee List**\n");
+        System.out.printf("%-10s %-25s %-15s %-15s %-20s %n", "Donee ID", "Donee Name", "Donee Phone", "Donee Urgency", "Registered Date");
+        System.out.println(String.format("%0" + 85 + "d", 0).replace("0", "-"));
+    }
+
+    public static void filterHighUrgencyUI(){
+        System.out.println("\n**High Urgency Donee List**\n");
+        System.out.printf("%-10s %-25s %-15s %-15s %-20s %n", "Donee ID", "Donee Name", "Donee Phone", "Donee Urgency", "Registered Date");
+        System.out.println(String.format("%0" + 85 + "d", 0).replace("0", "-"));
+    }
 }
--- src/main/java/Control/DoneeFilter.java
+++ src/main/java/Control/DoneeFilter.java
@@ -1,5 +1,198 @@
 package Control;
 
-public class DoneeFilter {
+import Boundary.DoneeUI;
 
+import Entity.Donee;
+
+import Libraries.ArrayList;
+import Libraries.GeneralFunction;
+
+import Utilities.Message;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class DoneeFilter {
+    public static void filterByType(ArrayList<Donee> donees) {
+        int filterType;
+
+        do {
+            while (true) { //Input Validation Loop
+                String filterTypeInput = DoneeUI.filterDoneeTypeUI(); // Read the input as a string and trim any surrounding spaces
+
+                if (filterTypeInput.isEmpty()) {
+                    Message.displayEmptyInputMessage("Please enter a number between 1 and 4.");
+                } else {
+                    try {
+                        filterType = Integer.parseInt(filterTypeInput);
+                        if (filterType >= 1 && filterType <= 4) {
+                            break;  // Exit loop for valid choice input
+                        } else {
+                            Message.displayInvalidChoiceMessage("Please enter a number between 1 and 4.");
+                        }
+                    } catch (NumberFormatException e) {  // Catch any non-integer inputs
+                        Message.displayInvalidInputMessage("Please enter a number.");
+                    }
+                }
+            }
+
+            switch (filterType) {
+                case 1:
+                    individualFilter(donees);
+                    break;
+                case 2:
+                    organizationFilter(donees);
+                    break;
+                case 3:
+                    familyFilter(donees);
+                    break;
+                default:
+                    GeneralFunction.enterToContinue();
+            }
+
+            if (filterType != 4) {
+                GeneralFunction.enterToContinue();
+            }
+        } while (filterType != 4);
+    }
+
+    public static void individualFilter(ArrayList<Donee> donees){
+        boolean found = false;
+
+        DoneeUI.individualFilterUI();
+        for (Donee donee : donees) {
+            if (Objects.equals(donee.getDoneeType(), "Individual")) {
+                DoneeUI.displayFilterDetailsUI(donee.getDoneeID(), donee.getName(), donee.getPhone(), donee.getDoneeType(), donee.getRegisteredDate());
+                found = true;
+            }
+        }
+        if (!found) {
+            Message.displayGeneralMessage("No donees found within the specified donee type.\n");
+        }
+        GeneralFunction.repeatPrint("-", 85);
+    }
+
+    public static void organizationFilter(ArrayList<Donee> donees){
+        boolean found = false;
+
+        DoneeUI.organizationFilterUI();
+        for (Donee donee : donees) {
+            if (Objects.equals(donee.getDoneeType(), "Organization")) {
+                DoneeUI.displayFilterDetailsUI(donee.getDoneeID(), donee.getName(), donee.getPhone(), donee.getDoneeType(), donee.getRegisteredDate());
+                found = true;
+            }
+        }
+        if (!found) {
+            Message.displayGeneralMessage("No donees found within the specified donee type.\n");
+        }
+        GeneralFunction.repeatPrint("-", 85);
+    }
+
+    public static void familyFilter(ArrayList<Donee> donees){
+        boolean found = false;
+
+        DoneeUI.familyFilterUI();
+        for (Donee donee : donees) {
+            if (Objects.equals(donee.getDoneeType(), "Family")) {
+                DoneeUI.displayFilterDetailsUI(donee.getDoneeID(), donee.getName(), donee.getPhone(), donee.getDoneeType(), donee.getRegisteredDate());
+                found = true;
+            }
+        }
+        if (!found) {
+            Message.displayGeneralMessage("No donees found within the specified donee type.\n");
+        }
+        GeneralFunction.repeatPrint("-", 85);
+    }
+
+    public static void filterByUrgency(ArrayList<Donee> donees){
+        int filterUrgency;
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            while (true) { //Input Validation Loop
+                String filterUrgencyInput = DoneeUI.filterUrgencyTypeUI();  // Read the input as a string and trim any surrounding spaces
+
+                if (filterUrgencyInput.isEmpty()) {
+                    Message.displayEmptyInputMessage("Please enter a number between 1 and 4.");
+                } else {
+                    try {
+                        filterUrgency = Integer.parseInt(filterUrgencyInput);
+                        if (filterUrgency >= 1 && filterUrgency <= 4) {
+                            break;  // Exit loop for valid choice input
+                        } else {
+                            Message.displayInvalidChoiceMessage("Please enter a number between 1 and 4.");
+                        }
+                    } catch (NumberFormatException e) {  // Catch any non-integer inputs
+                        Message.displayInvalidInputMessage("Please enter a number.");
+                    }
+                }
+            }
+
+            switch (filterUrgency) {
+                case 1:
+                    filterLowUrgency(donees);
+                    break;
+                case 2:
+                    filterModerateUrgency(donees);
+                    break;
+                case 3:
+                    filterHighUrgency(donees);
+                    break;
+                default:
+                    GeneralFunction.enterToContinue();
+            }
+
+            if (filterUrgency != 4) {
+                GeneralFunction.enterToContinue();
+            }
+        } while (filterUrgency != 4);
+    }
+
+    public static void filterLowUrgency(ArrayList<Donee> donees){
+        boolean found = false;
+
+        DoneeUI.filterLowUrgencyUI();
+        for (Donee donee : donees) {
+            if (Objects.equals(donee.getDoneeUrgency(), "Low")) {
+                DoneeUI.displayFilterDetailsUI(donee.getDoneeID(), donee.getName(), donee.getPhone(), donee.getDoneeUrgency(), donee.getRegisteredDate());
+                found = true;
+            }
+        }
+        if (!found) {
+            Message.displayGeneralMessage("No donees found within the specified urgency category.\n");
+        }
+        GeneralFunction.repeatPrint("-", 85);
+    }
+
+    public static void filterModerateUrgency(ArrayList<Donee> donees){
+        boolean found = false;
+
+        DoneeUI.filterModerateUrgencyUI();
+        for (Donee donee : donees) {
+            if (Objects.equals(donee.getDoneeUrgency(), "Moderate")) {
+                DoneeUI.displayFilterDetailsUI(donee.getDoneeID(), donee.getName(), donee.getPhone(), donee.getDoneeUrgency(), donee.getRegisteredDate());
+                found = true;
+            }
+        }
+        if (!found) {
+            Message.displayGeneralMessage("No donees found within the specified urgency category.\n");
+        }
+        GeneralFunction.repeatPrint("-", 85);
+    }
+
+    public static void filterHighUrgency(ArrayList<Donee> donees){
+        boolean found = false;
+
+        DoneeUI.filterHighUrgencyUI();
+        for (Donee donee : donees) {
+            if (Objects.equals(donee.getDoneeUrgency(), "High")) {
+                DoneeUI.displayFilterDetailsUI(donee.getDoneeID(), donee.getName(), donee.getPhone(), donee.getDoneeUrgency(), donee.getRegisteredDate());
+                found = true;
+            }
+        }
+        if (!found) {
+            Message.displayGeneralMessage("No donees found within the specified urgency category.\n");
+        }
+        GeneralFunction.repeatPrint("-", 85);
+    }
 }
