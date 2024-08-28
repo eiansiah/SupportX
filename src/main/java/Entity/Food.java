bad953567b3076b1613e2b22153a9d5ec6bf55f5
vivian <kojq-wp22@student.tarc.edu.my>
2024-08-29 02:00:28 +0800

    Entity Class

---
+++ src/main/java/Entity/Food.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entity;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Ko Jie Qi
+ */
+public class Food extends DonationItem{
+    private double netWeight;
+    private LocalDate expiryDate;
+    private String foodType;
+    private String venueCode;
+
+    public Food(double netWeight, LocalDate expiryDate, String foodType, String venueCode, String itemCode, String itemName, int Quantity) {
+        super(itemCode, itemName, Quantity);
+        this.netWeight = netWeight;
+        this.expiryDate = expiryDate;
+        this.foodType = foodType;
+        this.venueCode = venueCode;
+    }
+
+    public double getNetWeight() {
+        return netWeight;
+    }
+
+    public void setNetWeight(double netWeight) {
+        this.netWeight = netWeight;
+    }
+
+    public LocalDate getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(LocalDate expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public String getFoodType() {
+        return foodType;
+    }
+
+    public void setFoodType(String foodType) {
+        this.foodType = foodType;
+    }
+
+    public String getVenueCode() {
+        return venueCode;
+    }
+
+    public void setVenueCode(String venueCode) {
+        this.venueCode = venueCode;
+    }
+
+    @Override
+    public String toString() {
+        return "\nFood\n"+super.toString() + "\nNet Weight: " + netWeight + "\nExpiry Date: " + expiryDate + "\nFood Type: " + foodType + "\nVenue Code: " + venueCode + '\n';
+    }
+    
+    
+
+}
---
+++ src/main/java/Entity/MedicalDevice.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entity;
+
+/**
+ *
+ * @author Ko Jie Qi
+ */
+public class MedicalDevice extends DonationItem{
+    private String venueCode;
+    private String description;
+
+    public MedicalDevice(String venueCode, String description, String itemCode, String itemName, int Quantity) {
+        super(itemCode, itemName, Quantity);
+        this.venueCode = venueCode;
+        this.description = description;
+    }
+    
+    public String getVenueCode() {
+        return venueCode;
+    }
+
+    public void setVenueCode(String venueCode) {
+        this.venueCode = venueCode;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "\nMedicine Device\n"+super.toString() + "\nVenue Code: " + venueCode + "\nDescription: " + description + '\n';
+    }
+    
+}
